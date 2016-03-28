94b18d11a39f664914def2fc88ac41db8213900e
Jake Runzer <jakerunzer@gmail.com>
2016-03-28 16:24:48 -0700

    final code

--- Final Code/eliminate.m
+++ Final Code/eliminate.m
@@ -8,13 +8,13 @@ function [ objects, count ] = eliminate( global_objects, global_count )
 
     % How much the copepod needs to move to be considered a copepod
     % Choosen by experimentation
-    MOVE_THRESH = 20;
+    MOVE_THRESH = 10;
 
     % Loop through all counted objects and check if they have moved
     for i=1:global_count
         cur_copepod = global_objects(i);
         moved_amount = sum(cur_copepod.deltas);
-        if moved_amount > MOVE_THRESH
+        if moved_amount > MOVE_THRESH * cur_copepod.num_locations
            objects(count) = cur_copepod;
            count = count + 1;
         end
--- Final Code/main.m
+++ Final Code/main.m
@@ -5,7 +5,7 @@ close all;
 video = 'copepod_video.mp4';
 
 % Number of frame process
-number_of_frames = 20;
+number_of_frames = 4;
 
 % Run Copepod Tracking
 copepod_count = Copepods(video, number_of_frames);
